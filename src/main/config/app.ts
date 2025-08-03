ccdcc5430e6577d63bfd9ffd51f722d59699129a
Eduardo Nogueira <eduardodudunog@gmail.com>
2025-08-03 14:18:55 -0300

    chore: setup routes

--- src/main/config/app.ts
+++ src/main/config/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
 import setupMiddlewares from "./middlewares";
+import setupRoutes from "./routes";
 
 const makeApp = async () => {
   const app = express();
   setupMiddlewares(app);
+  setupRoutes(app);
 
   return app;
 };
